37700638ddfc7e81a29903a04e436f3f7f8199ef
Razzak118348 <abdurrazzak118348@gmail.com>
2026-04-22 23:48:53 +0600

    feat: vector Problem file added

---
+++ vectorProblem.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v = {2,4,5,5,7,7,1};
+    sort(v.begin(),v.end());
+    unique(v.begin(),v.end());
+
+    for(auto u:v) cout<<u<<" ";
+    return 0;
+}
