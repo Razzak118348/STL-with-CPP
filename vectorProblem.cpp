365aaa2dc61c787c7e43b870e0f8e57a7c594d75
Razzak118348 <abdurrazzak118348@gmail.com>
2026-04-25 00:16:28 +0600

    vector Problem update

--- vectorProblem.cpp
+++ vectorProblem.cpp
@@ -1,10 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> v = {2,4,5,5,7,7,1};
-    sort(v.begin(),v.end());
-    unique(v.begin(),v.end());
+    vector<int> v = {2,4,4,5,5,7,7,1};
+    sort(v.begin(),v.end());//o(nlogn)
 
-    for(auto u:v) cout<<u<<" ";
+int Sz =unique(v.begin(),v.end())-v.begin();//O(n)
+    cout<<Sz<<endl;
+for(int i=0;i<Sz;i++){
+    cout<<v[i]<<" ";
+}
+
     return 0;
 }
